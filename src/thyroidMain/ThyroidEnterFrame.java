03664e310879353bbb2279096edcecfa81252ae5
Woon Jung Koh <jjkoh21@gmail.com>
2023-03-04 11:41:59 +0900

    https://github.com/MigoJJ/ThyroidJFrameButton.git

--- src/thyroidMain/ThyroidEnterFrame.java
+++ src/thyroidMain/ThyroidEnterFrame.java
@@ -5,40 +5,36 @@ import java.awt.event.*;
 import javax.swing.*;
 
 class ThyroidEnterFrame extends JFrame {
-	private JTextField textField1;
-
 	ThyroidEnterFrame()	{
-		// Creating Object of Jpanel class
 		JPanel pa = new JPanel();
-		// set the layout
 		pa.setLayout(new BorderLayout());
 		// add a new JButton with name "wel" and it is
 		pa.add(new JButton("WelCome"), BorderLayout.NORTH);
-		// add a new JButton with name "come" and it is
-
-		// Add the buttons to the panel
-			 JPanel buttonPanel = new JPanel();
-			 String[] buttonArray = {"apple", "banana", "cherry", "date"};
-			 for (int i = 0; i < buttonArray.length; i++) {
-			     JButton button = tsub_MakeButtons.createButton(buttonArray[i]);
-			     buttonPanel.add(button); // add the button to the panel
-			 }
-			 pa.add(buttonPanel, BorderLayout.SOUTH);
-//		pa.add(new JButton("Button1"), BorderLayout.SOUTH);
-
 		pa.add(new JButton("Layout"), BorderLayout.EAST);
 		pa.add(new JButton("Border"), BorderLayout.WEST);
-		// add a new JButton with name "hello everybody" and it is
-			textField1 = new JTextField(20);
-			JPanel textFieldPanel = new JPanel();
-			textFieldPanel.add(textField1);
-			pa.add(textFieldPanel, BorderLayout.CENTER);
 		
-		// add the pa object which refer to the Jpanel
+		// Add the buttons to the panel
+			JPanel buttonPanel = new JPanel();
+			String[] buttonArray = {"apple", "banana", "cherry", "date"};
+			for (int i = 0; i < buttonArray.length; i++) {
+			     JButton button = tsub_MakeButtons.createButton(buttonArray[i]);
+			     buttonPanel.add(button); // add the button to the panel
+			}
+
+			JPanel textFieldPanel = new JPanel();
+			JTextField textField = tsub_MakeButtons.createTextField();
+			textFieldPanel.add(textField);
+			
+		pa.add(buttonPanel, BorderLayout.SOUTH);
+		pa.add(textFieldPanel, BorderLayout.SOUTH);
+		
+		add(buttonPanel, BorderLayout.SOUTH);
+//		add(textFieldPanel, BorderLayout.SOUTH);
 		add(pa);
+
 		// Function to close the operation of JFrame.
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setSize(600, 600);
+		setSize(1600, 600);
 		setVisible(true);
 	}
 	public static void main(String[] args)	{
--- src/thyroidMain/tsub_MakeButtons.java
+++ src/thyroidMain/tsub_MakeButtons.java
@@ -1,16 +1,29 @@
 package thyroidMain;
 
-import javax.swing.JButton;
+import java.awt.Font;
 
-public class tsub_MakeButtons {
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class tsub_MakeButtons extends ThyroidEnterFrame {
     
 	public static JButton createButton(String label) {
-        // Create the button
         JButton button = new JButton(label);
-        
-        // Set the size and location of the button (change as needed)
         button.setBounds(100, 50, 100, 30);
-        
         return button;
+	}
+	
+	public static JTextField createTextField() {
+		JTextField textField = new JTextField("Enter text here");
+			
+        Font font = new Font("Arial", Font.PLAIN, 18); // create a new font object with size 18
+        textField.setFont(font);
+
+        textField.setColumns(40); // set the preferred number of columns to 20
+			textField.setBounds(50, 50, 200, 130);
+			textField.setVisible(true);
+		return textField;
     }
 }
